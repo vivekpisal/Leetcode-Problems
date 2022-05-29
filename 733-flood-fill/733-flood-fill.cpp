21a00744fb1ded7362eb807cf4360fc2c912e7ba
Vivek Pisal <54746512+vivekpisal@users.noreply.github.com>
2022-05-29 11:50:28 +0530

    Time: 16 ms (34.02%), Space: 14 MB (51.15%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    void ff(vector<vector<int>> &image,int sr,int sc,int newc,vector<vector<bool>> &mp,int color){
-        if(sr < 0 || sc < 0 || sr >= image.size() || sc >= image[0].size() || image[sr][sc] != color || mp[sr][sc] == true){
+    void ff(vector<vector<int>> &image,int sr,int sc,int newc,int color){
+        if(sr < 0 || sc < 0 || sr >= image.size() || sc >= image[0].size() || image[sr][sc] != color){
             return;
         }
         
         image[sr][sc]=newc;
-        mp[sr][sc]=true;
-        ff(image,sr+1,sc,newc,mp,color);
-        ff(image,sr-1,sc,newc,mp,color);
-        ff(image,sr,sc+1,newc,mp,color);
-        ff(image,sr,sc-1,newc,mp,color);
+        ff(image,sr+1,sc,newc,color);
+        ff(image,sr-1,sc,newc,color);
+        ff(image,sr,sc+1,newc,color);
+        ff(image,sr,sc-1,newc,color);
     }
     
     
     
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-        vector<vector<bool>> mp(image.size(),vector<bool> (image[0].size(),false));
-        ff(image,sr,sc,newColor,mp,image[sr][sc]);
+        if(image[sr][sc] == newColor)   return image;
+        ff(image,sr,sc,newColor,image[sr][sc]);
         return image;
     }
 };
