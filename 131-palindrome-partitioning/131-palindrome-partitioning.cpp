fe3e33175ef08226a0b018560b7858b244682899
Vivek Pisal <54746512+vivekpisal@users.noreply.github.com>
2022-08-11 19:06:51 +0530

    Time: 185 ms (63.46%), Space: 49.1 MB (96.64%) - LeetHub

---
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool check_pali(string s){
+        int i=0,j=s.size()-1;
+        while(j >= i){
+            if(s[i++] != s[j--]){
+                return false;
+            }
+        }
+        return true;
+    }
+    void paliPartition(string s,vector<string>& atn,vector<vector<string>>& ans)
+    {
+        if(s.size() == 0){
+            ans.push_back(atn);
+            return;
+        }
+        
+        for(int j=0;j<s.size();j++){
+            string sub=s.substr(0,j+1);
+            if(check_pali(sub)){
+                atn.push_back(sub);
+                s.erase(0,j+1);
+                paliPartition(s,atn,ans);
+                atn.pop_back();
+                s=sub+s;
+            }
+        }
+    }
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> ans;
+        vector<string> atn;
+        paliPartition(s,atn,ans);
+        return ans;
+    }
+};
