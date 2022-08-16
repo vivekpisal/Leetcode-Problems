77f74617e669e5d4c0355406f45120462b5bc9d5
Vivek Pisal <54746512+vivekpisal@users.noreply.github.com>
2022-08-16 11:53:20 +0530

    Time: 18 ms (37.94%), Space: 14.4 MB (11.05%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
-    void ff(vector<vector<int>> &image,int sr,int sc,int newc,int color){
-        if(sr < 0 || sc < 0 || sr >= image.size() || sc >= image[0].size() || image[sr][sc] != color){
-            return;
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color)     {
+        queue<pair<int,int>> q;
+        q.push({sr,sc});
+        int n=image.size();
+        int m=image[0].size();
+        vector<vector<bool>> vis(n,vector<bool>(m,false));
+        int oldC=image[sr][sc];
+        vis[sr][sc]=true;
+        while(!q.empty()){
+            int row=q.front().first;
+            int col=q.front().second;
+            q.pop();
+            if(col+1 < m && image[row][col+1] == oldC && vis[row][col+1] == false)
+                {q.push({row,col+1});vis[row][col+1]=true;}
+            
+            if(col-1 >= 0 && image[row][col-1] == oldC && vis[row][col-1] == false)
+                {q.push({row,col-1});vis[row][col-1]=true;}
+            
+            if(row + 1 < n && image[row+1][col] == oldC && vis[row+1][col] == false)
+                {q.push({row+1,col});vis[row+1][col]=true;}
+            
+            if(row-1 >= 0 && image[row-1][col] == oldC && vis[row-1][col] == false)
+                {q.push({row-1,col});vis[row-1][col]=true;}
+            
+            image[row][col]=color;
         }
-        
-        image[sr][sc]=newc;
-        ff(image,sr+1,sc,newc,color);
-        ff(image,sr-1,sc,newc,color);
-        ff(image,sr,sc+1,newc,color);
-        ff(image,sr,sc-1,newc,color);
-    }
-    
-    
-    
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-        if(image[sr][sc] == newColor)   return image;
-        ff(image,sr,sc,newColor,image[sr][sc]);
         return image;
     }
 };
