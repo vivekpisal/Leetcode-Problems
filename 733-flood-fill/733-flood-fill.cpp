f1efda0248428fb484dd81fae4a94160f59fd827
Vivek Pisal <54746512+vivekpisal@users.noreply.github.com>
2022-08-16 14:55:53 +0530

    Time: 7 ms (95.33%), Space: 13.8 MB (98.78%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,31 +1,27 @@
 class Solution {
-public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color)     {
-        queue<pair<int,int>> q;
-        q.push({sr,sc});
+private:
+    void dfs(vector<vector<int>> &image,int sr,int sc,int delRow[],int delCol[],int color,int oldColor)
+    {
         int n=image.size();
         int m=image[0].size();
-        vector<vector<bool>> vis(n,vector<bool>(m,false));
-        int oldC=image[sr][sc];
-        vis[sr][sc]=true;
-        while(!q.empty()){
-            int row=q.front().first;
-            int col=q.front().second;
-            q.pop();
-            if(col+1 < m && image[row][col+1] == oldC && vis[row][col+1] == false)
-                {q.push({row,col+1});vis[row][col+1]=true;}
-            
-            if(col-1 >= 0 && image[row][col-1] == oldC && vis[row][col-1] == false)
-                {q.push({row,col-1});vis[row][col-1]=true;}
-            
-            if(row + 1 < n && image[row+1][col] == oldC && vis[row+1][col] == false)
-                {q.push({row+1,col});vis[row+1][col]=true;}
-            
-            if(row-1 >= 0 && image[row-1][col] == oldC && vis[row-1][col] == false)
-                {q.push({row-1,col});vis[row-1][col]=true;}
-            
-            image[row][col]=color;
+        image[sr][sc]=color;
+        for(int i=0;i<4;i++){
+            int nrow=sr + delRow[i];
+            int ncol=sc + delCol[i];
+            if(nrow >= 0 && nrow < n && ncol>= 0 && ncol < m && image[nrow][ncol] == oldColor){
+                dfs(image,nrow,ncol,delRow,delCol,color,oldColor);
+            }
         }
+    }
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color)     {
+        if(image[sr][sc] == color)
+            return image;
+        int delRow[]={-1,0,1,0};
+        int delCol[]={0,1,0,-1};
+        int oldColor=image[sr][sc];
+        //vector<vector<bool>> vis(image.size(),vector<bool> (image[0].size(),false));
+        dfs(image,sr,sc,delRow,delCol,color,oldColor);
         return image;
     }
 };
