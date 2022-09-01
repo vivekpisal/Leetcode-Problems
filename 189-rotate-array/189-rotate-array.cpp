21b8f665b6e440aec3b3ea8cd1d18caee98c2232
Vivek Pisal <54746512+vivekpisal@users.noreply.github.com>
2022-09-01 18:50:55 +0530

    Time: 55 ms (34.77%), Space: 25.7 MB (11.59%) - LeetHub

---
+++ 189-rotate-array/189-rotate-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        vector<int> ans(nums.size());
+        for(int i=0;i<nums.size();i++){
+            ans[(i+k)%nums.size()]=nums[i];
+        }
+        for(int i=0;i<nums.size();i++){
+            nums[i]=ans[i];
+        }
+    }
+};
