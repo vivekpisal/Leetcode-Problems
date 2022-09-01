f603a981ba27a3e871e74a6ea1ca648453d9a67e
Vivek Pisal <54746512+vivekpisal@users.noreply.github.com>
2022-09-01 16:33:42 +0530

    Time: 33 ms (66.17%), Space: 19.2 MB (57.59%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -16,9 +16,7 @@ public:
         int lastnonzero=0;
         for(int i=0;i<nums.size();i++){
             if(nums[i] != 0)
-                nums[lastnonzero++]=nums[i];
+                swap(nums[lastnonzero++],nums[i]);
         }
-        for(int i=lastnonzero;i<nums.size();i++)
-            nums[i]=0;
     }
 };
