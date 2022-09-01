cec5fcead19a2356eb7076f492b8a164b68fc2b3
Vivek Pisal <54746512+vivekpisal@users.noreply.github.com>
2022-09-01 16:04:11 +0530

    Time: 35 ms (60.25%), Space: 19.1 MB (98.05%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int z=0;
-        int n=0;
-        while(z < nums.size()){
-            if(nums[z] == 0){
-                n=z+1;
-                while(n < nums.size() && nums[n] == 0){n++;}
+//         int z=0;
+//         int n=0;
+//         while(z < nums.size()){
+//             if(nums[z] == 0){
+//                 n=z+1;
+//                 while(n < nums.size() && nums[n] == 0){n++;}
                 
-                if(n < nums.size())
-                    swap(nums[n],nums[z]);
-            }
-            z++;
+//                 if(n < nums.size())
+//                     swap(nums[n],nums[z]);
+//             }
+//             z++;
+//         }
+        int lastnonzero=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i] != 0)
+                nums[lastnonzero++]=nums[i];
         }
-        
+        for(int i=lastnonzero;i<nums.size();i++)
+            nums[i]=0;
     }
 };
