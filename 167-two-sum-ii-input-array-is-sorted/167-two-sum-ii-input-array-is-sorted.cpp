23e92ceaaafda0ad355466d609ee8835455efa8e
Vivek Pisal <54746512+vivekpisal@users.noreply.github.com>
2022-09-09 12:15:03 +0530

    Time: 46 ms (5.48%), Space: 15.7 MB (44.10%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,16 +1,26 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& numbers, int target) {
-        int low=0,high=numbers.size()-1;
-        vector<int> ans;
+    int bSearch(vector<int> &numbers,int target,int low,int high){
         while(low <= high){
-            int sumOfTwo=numbers[low] + numbers[high];
-            if(sumOfTwo == target)
-                {ans.push_back(low+1);ans.push_back(high+1);break;}
-            else if(sumOfTwo > target)
-                high=high-1;
-            else
-                low=low+1;
+            int mid=low + (high-low)/2;
+            if(numbers[mid] == target)
+                return mid;
+            else if(numbers[mid] > target)
+                high=mid-1;
+            else if(target > numbers[mid])
+                low=mid+1;
+        }
+        return 0;
+    }
+    
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        vector<int> ans;
+        for(int i=0;i<numbers.size();i++){
+            int pos=bSearch(numbers,target-numbers[i],i+1,numbers.size()-1);
+            if(pos){
+                ans.push_back(i+1);
+                ans.push_back(pos+1);
+            }
         }
         return ans;
     }
