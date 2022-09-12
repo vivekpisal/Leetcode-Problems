4da3f39514d47fffa280b0d020a5b7e76c0159ae
Vivek Pisal <54746512+vivekpisal@users.noreply.github.com>
2022-09-12 15:18:19 +0530

    Time: 154 ms (63.41%), Space: 49.9 MB (17.27%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,7 +1,16 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        sort(nums.begin(),nums.end(),greater<int>());
-        return nums[k-1]; 
+        priority_queue<int> maxHeap;
+        for(int i:nums){
+            maxHeap.push(i);
+        }
+        int kthLar=-1;
+        while(k){
+            kthLar=maxHeap.top();
+            maxHeap.pop();
+            k--;
+        }
+        return kthLar;
     }
 };
