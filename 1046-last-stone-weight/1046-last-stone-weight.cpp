b3489994c4b3a3f7e8427acd0dd8c98913ed5454
Vivek Pisal <54746512+vivekpisal@users.noreply.github.com>
2022-09-13 13:14:15 +0530

    Time: 4 ms (39.02%), Space: 7.7 MB (32.13%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        int lastStone=0;
+        priority_queue<int> maxH;
+        for(int i:stones){
+            maxH.push(i);
+        }
+        while(maxH.size() > 1){
+            int y=maxH.top();
+            maxH.pop();
+            int x=maxH.top();
+            maxH.pop();
+            if(y!=x)
+                maxH.push(y-x);
+        }
+        if(maxH.size() == 1)
+            lastStone=maxH.top();
+        return lastStone;
+    }
+};
