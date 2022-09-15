3dacbeff0208932fd982d7cb5617adf76d508590
Vivek Pisal <54746512+vivekpisal@users.noreply.github.com>
2022-09-15 12:00:53 +0530

    Time: 13 ms (11.81%), Space: 9.6 MB (51.35%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,28 +1,16 @@
 class Solution {
-private:
-    bool binSearch(vector<int> &arr,int target){
-        int low=0,high=arr.size()-1;
-        while(low <= high){
-            int mid=(low + high)/2;
-            if(arr[mid] == target)
-                return true;
-            else if(arr[mid] > target)
-                high=mid-1;
-            else if(arr[mid] < target)
-                low=mid+1;
-        }
-        return false;
-    }
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         if(matrix.size() == 0)
             return false;
-        int lastColIndex=matrix[0].size()-1;
-        for(int i=0;i<matrix.size();i++){
-            if(matrix[i][0] <= target && matrix[i][lastColIndex] >= target){
-                if(binSearch(matrix[i],target))
-                    return true;
-            }
+        int i=0,j=matrix[0].size()-1;
+        while(i < matrix.size() && j >= 0){
+            if(matrix[i][j] == target)
+                return true;
+            else if(matrix[i][j] < target)
+                i++;
+            else if(matrix[i][j] > target)
+                j--;
         }
         return false;
     }
