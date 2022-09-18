58f81d32464e1f717bd84ea1d5b65de9f6241f2d
Vivek Pisal <54746512+vivekpisal@users.noreply.github.com>
2022-09-18 18:24:02 +0530

    Time: 12 ms (64.47%), Space: 10.8 MB (99.11%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -2,6 +2,9 @@ class Solution {
 public:
     vector<vector<int>> ans;
     void Csum(vector<int>& arr,vector<int>& ds,int tar,int i){
+        if(tar < 0)
+            return;
+        
         if(i == arr.size()){
             if(0 == tar){
                 ans.push_back(ds);
@@ -9,12 +12,10 @@ public:
             return;
         }
         
-        if(tar >= arr[i])
-        {
-            ds.push_back(arr[i]);
-            Csum(arr,ds,tar-arr[i],i);
-            ds.pop_back();
-        }
+
+        ds.push_back(arr[i]);
+        Csum(arr,ds,tar-arr[i],i);
+        ds.pop_back();
         
         Csum(arr,ds,tar,i+1);
     }
