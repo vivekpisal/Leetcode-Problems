5b666d6853dde8a12fe1c465bd10c166e6ae1d60
Vivek Pisal <54746512+vivekpisal@users.noreply.github.com>
2022-10-04 16:28:07 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (98.66%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
-    void allPermutations(vector<vector<int>> &ans,unordered_map<int,bool> &mp,vector<int> &nums,vector<int> &ds,int i){
+    void allPermutations(vector<vector<int>> &ans,vector<int> &nums,int i){
         if(i == nums.size()){
-            ans.push_back(ds);
+            ans.push_back(nums);
             return;
         }
         
-        for(int ind=0;ind<nums.size();ind++){
-            if(mp.find(nums[ind]) == mp.end()){
-                mp[nums[ind]]=false;
-            }
-            if(mp[nums[ind]] == false){
-                ds.push_back(nums[ind]);
-                mp[nums[ind]]=true;
-                allPermutations(ans,mp,nums,ds,i+1);
-                ds.pop_back();
-                mp[nums[ind]]=false;
-            }
+        for(int j=i;j<nums.size();j++){
+            swap(nums[j],nums[i]);
+            allPermutations(ans,nums,i+1);
+            swap(nums[j],nums[i]);
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
-        unordered_map<int,bool> mp;
         vector<vector<int>> ans;
-        vector<int> ds;
-        allPermutations(ans,mp,nums,ds,0);
+        allPermutations(ans,nums,0);
         return ans;
     }
 };
