04d4215fbd22652e0cac9c5966c98a97f9fb520b
Vivek Pisal <54746512+vivekpisal@users.noreply.github.com>
2022-10-05 12:15:14 +0530

    Time: 67 ms (25.38%), Space: 6.4 MB (51.81%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.cpp
+++ 37-sudoku-solver/37-sudoku-solver.cpp
@@ -1,55 +1,57 @@
 class Solution {
 public:
-    bool can_put(vector<vector<char>>& board,int row,int col,char n){
-        for(int i=0;i<board.size();i++){
-            if(board[i][col] == n)
-                return false;
-            if(board[row][i] == n)
+    bool checkBoard(vector<vector<char>> &board,int row,int col,char p){
+        for(int i=0;i<board[row].size();i++){
+            if(board[row][i] == p)
                 return false;
         }
-        int srow=(row/3)*3;
-        int scol=(col/3)*3;
-        for(int i=srow;i<srow+3;i++){
-            for(int j=scol;j<scol+3;j++){
-                if(board[i][j] == n)
+        for(int i=0;i<board.size();i++){
+            if(board[i][col] == p)
+                return false;
+        }
+        
+        int brow=(row/3)*3;
+        int bcol=(col/3)*3;
+        for(int i=brow;i<brow+3;i++){
+            for(int j=bcol;j<bcol+3;j++){
+                if(board[i][j] == p)
                     return false;
             }
         }
         return true;
     }
-    bool sudokuSolve(vector<vector<char>>& board,int row,int col){
-        if(row == board.size())
+    bool sudokuSolver(vector<vector<char>> &board,int row,int col){
+        if(row == board.size()){
             return true;
+        }
         
-        int nr,nc;
-        if(col == board.size()-1){
-            nr=row+1;
-            nc=0;
+        
+        int nrow,ncol;
+        if(col == board[0].size()-1)
+        {
+            nrow=row+1;
+            ncol=0;
         }
         else{
-            nr=row;
-            nc=col+1;
+            nrow=row;
+            ncol=col+1;
         }
-        
-        if(board[row][col] != '.')
-            return sudokuSolve(board,nr,nc);
-        else
-        {
-            for(char no='1';no<='9';no++)
-            {
-                if(can_put(board,row,col,no) == true)
-                {
-                    board[row][col]=no;
-                    if(sudokuSolve(board,nr,nc))
+        if(board[row][col] != '.'){
+            return sudokuSolver(board,nrow,ncol);
+        }
+        else{
+            for(char p='1';p<='9';p++){
+                if(checkBoard(board,row,col,p)){
+                    board[row][col]=p;
+                    if(sudokuSolver(board,nrow,ncol))
                         return true;
-                    else
-                        board[row][col]='.';
-                } 
+                    board[row][col]='.';
+                }
             }
         }
         return false;
     }
     void solveSudoku(vector<vector<char>>& board) {
-        sudokuSolve(board,0,0);
+        sudokuSolver(board,0,0);
     }
 };
