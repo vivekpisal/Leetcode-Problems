c7cba841208cb1c9359b9762981acf6a9bbf6efd
Vivek Pisal <54746512+vivekpisal@users.noreply.github.com>
2024-12-15 10:54:56 +0530

    Time: 2 ms (99.41%), Space: 48.7 MB (6.19%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,32 +1,32 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int cnt1=0,cnt2=0;
-        int ele1=0,ele2=0;
+        int cnt1=0,ele1=0,cnt2=0,ele2=0;
         List<Integer> ans = new ArrayList<>();
         for(int i=0;i<nums.length;i++){
-            if(cnt1 == 0 && ele2 != nums[i]){
-                ele1=nums[i];
+            if(nums[i] == ele1){
                 cnt1++;
-            }else if(cnt2 == 0 && ele1 != nums[i]){
-                ele2=nums[i];
+            }else if(nums[i] == ele2){
                 cnt2++;
-            }else if(ele1 == nums[i])
-                cnt1++;
-            else if(ele2 == nums[i])
-                cnt2++;
-            else{
+            }
+            else if(cnt1 == 0){
+                cnt1 = 1;
+                ele1 = nums[i];
+            }else if(cnt2 == 0){
+                ele2 = nums[i];
+                cnt2 = 1;
+            }else{
                 cnt1--;
                 cnt2--;
             }
         }
+        int minCount = (nums.length/3)+1;
         cnt1=0;cnt2=0;
         for(int i=0;i<nums.length;i++){
-            if(ele1 == nums[i]) cnt1++;
-            if(ele2 == nums[i]) cnt2++;
+            if(nums[i] == ele1) cnt1++;
+            if(nums[i] == ele2) cnt2++;
         }
-        int mini = (nums.length/3) + 1;
-        if(cnt1 != 0 && mini <= cnt1)ans.add(ele1);
-        if(cnt2 != 0 && ele1 != ele2 && mini <= cnt2)ans.add(ele2);
+        if(cnt1 >= minCount) ans.add(ele1);
+        if(cnt2 >= minCount && ele1 != ele2) ans.add(ele2);
         Collections.sort(ans);
         return ans;
     }
