ece4320540550cbe1b88f8c46c2f6d31ed2039c6
Vivek Pisal <54746512+vivekpisal@users.noreply.github.com>
2025-05-31 12:55:40 +0530

    Time: 2 ms (99.47%), Space: 50 MB (36.32%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,33 +1,36 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int cnt1=0,ele1=0,cnt2=0,ele2=0;
-        List<Integer> ans = new ArrayList<>();
+        int cnt1=0,cnt2=0;
+        int ele1=Integer.MIN_VALUE,ele2=Integer.MIN_VALUE;
+        List<Integer> list = new ArrayList<>();
         for(int i=0;i<nums.length;i++){
-            if(nums[i] == ele1){
-                cnt1++;
-            }else if(nums[i] == ele2){
-                cnt2++;
-            }
-            else if(cnt1 == 0){
-                cnt1 = 1;
+            if(cnt1 == 0 && ele2 != nums[i]){
+                cnt1=1;
                 ele1 = nums[i];
-            }else if(cnt2 == 0){
+            }
+            else if(cnt2 == 0 && ele1 != nums[i]){
+                cnt2=1;
                 ele2 = nums[i];
-                cnt2 = 1;
-            }else{
+            }
+            else if(ele2 == nums[i]) cnt2++;
+            else if(ele1 == nums[i]) cnt1++;
+            else {
                 cnt1--;
                 cnt2--;
             }
         }
-        int minCount = (nums.length/3)+1;
-        cnt1=0;cnt2=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i] == ele1) cnt1++;
-            if(nums[i] == ele2) cnt2++;
+        cnt1 = 0; cnt2 = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == ele1) cnt1++;
+            if (nums[i] == ele2) cnt2++;
         }
-        if(cnt1 >= minCount) ans.add(ele1);
-        if(cnt2 >= minCount && ele1 != ele2) ans.add(ele2);
-        Collections.sort(ans);
-        return ans;
+        int maxCountThen = (nums.length/3)+1;
+        if(cnt1 >= maxCountThen){
+            list.add(ele1);
+        }
+        if(cnt2 >= maxCountThen){
+            list.add(ele2);
+        }
+        return list;
     }
 }
