e12892727a84d864d6711be1d14d1e3ad9fc8380
Vivek Pisal <54746512+vivekpisal@users.noreply.github.com>
2025-07-09 19:51:14 +0530

    Time: 3 ms (75.91%), Space: 50.5 MB (41.91%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,36 +1,43 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
+        List<Integer> majEle = new ArrayList<>();
         int cnt1=0,cnt2=0;
         int ele1=Integer.MIN_VALUE,ele2=Integer.MIN_VALUE;
-        List<Integer> list = new ArrayList<>();
+        int minThre = (nums.length/3)+1;
         for(int i=0;i<nums.length;i++){
-            if(cnt1 == 0 && ele2 != nums[i]){
-                cnt1=1;
-                ele1 = nums[i];
+            int curr = nums[i];
+            if(cnt1 == 0 && ele1 != curr && ele2  != curr){
+                ele1 = curr;
+                cnt1++;
             }
-            else if(cnt2 == 0 && ele1 != nums[i]){
-                cnt2=1;
-                ele2 = nums[i];
+            else if(cnt2 == 0 && ele2  != curr && ele1 != curr){
+                ele2 = curr;
+                cnt2++;
             }
-            else if(ele2 == nums[i]) cnt2++;
-            else if(ele1 == nums[i]) cnt1++;
-            else {
+            else if(ele1 == curr){
+                cnt1++;
+            }
+            else if(ele2 == curr){
+                cnt2++;
+            }
+            else{
                 cnt1--;
                 cnt2--;
             }
         }
-        cnt1 = 0; cnt2 = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == ele1) cnt1++;
-            if (nums[i] == ele2) cnt2++;
+        cnt1=0;
+        cnt2=0;
+        for(int i=0;i<nums.length;i++){
+            int curr = nums[i];
+            if(ele1 == curr)
+                cnt1++;
+            else if(ele2 == curr)
+                cnt2++;
         }
-        int maxCountThen = (nums.length/3)+1;
-        if(cnt1 >= maxCountThen){
-            list.add(ele1);
-        }
-        if(cnt2 >= maxCountThen){
-            list.add(ele2);
-        }
-        return list;
+        if(minThre <= cnt1)
+            majEle.add(ele1);
+        if(minThre <= cnt2)
+            majEle.add(ele2);
+        return majEle;
     }
 }
