d8095c4a85b84f8eb33c78fef6afb0cff41e3da1
Vivek Pisal <54746512+vivekpisal@users.noreply.github.com>
2025-07-10 20:17:39 +0530

    Time: 314 ms (20.22%), Space: 51.4 MB (70.64%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,13 +1,13 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> sums = new ArrayList<>();
         Arrays.sort(nums);
+        int n=nums.length;
+        List<List<Integer>> threeSum = new ArrayList<>();
+        for(int i=0;i<n-2;i++){
+            while(i != 0 && i < n-2 && nums[i] == nums[i-1]) i++;
 
-        for(int i=0;i<nums.length-2;i++){
-            while(i != 0 && i<nums.length-2 && nums[i] == nums[i-1]) i++;
-            
-            for(int j=i+1;j<nums.length-1;j++){
-                while(j != i+1 && j<nums.length-1 && nums[j] == nums[j-1]) j++;
+            for(int j=i+1;j<n-1;j++){
+                while(j != i+1 && j < n-1 && nums[j] == nums[j-1]) j++;
 
                 int target = nums[i] + nums[j];
                 int start=j+1,end=nums.length-1,mid=(start+end)/2;
@@ -15,7 +15,7 @@ class Solution {
                 while(start <= end){
                     mid=(start+end)/2;
                     if(nums[mid] + target == 0){
-                        sums.add(List.of(nums[i],nums[j],nums[mid]));
+                        threeSum.add(List.of(nums[i],nums[j],nums[mid]));
                         break;
                     }
                     else if(nums[mid] + target > 0){
@@ -26,8 +26,7 @@ class Solution {
                     }
                 }
             }
-
         }
-        return sums;
+        return threeSum;
     }
 }
