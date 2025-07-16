18492f092272320e56ef1a65510e2b3098ff682d
Vivek Pisal <54746512+vivekpisal@users.noreply.github.com>
2025-07-16 21:22:47 +0530

    Time: 5 ms (55.53%), Space: 68.8 MB (31.83%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -1,31 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-    public boolean isPalindrome(ListNode head) {
-        ListNode fast=head,slow=head;
-        while(fast != null && fast.next != null){
-            fast=fast.next.next;
-            slow=slow.next;
-        }
-        if(fast != null)
-            slow=slow.next;
-        
-        slow = reverse(slow);
-        fast = head;
-        while(slow != null){
-            if(slow.val != fast.val) return false;
-            slow=slow.next;
-            fast=fast.next;
-        }
-        return true;
-    }
-    
-    private ListNode reverse(ListNode head){
-        ListNode prev=null,curr=head;
+    public ListNode reverse(ListNode head){
+        ListNode prev = null,curr = head,next = head.next;
         while(curr != null){
-            ListNode next = curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr=next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+            if(next != null) next = next.next;
         }
         return prev;
+    }
+
+    public boolean isPalindrome(ListNode head) {
+        if(head == null || head.next == null) return true;
+
+        ListNode slow = head,fast = head;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        if(fast != null)
+            fast = slow.next;
+
+        fast = head;
+        slow = reverse(slow);
+        while(slow != null){
+            if(slow.val != fast.val)
+                return false;
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return true;
     }
 }
