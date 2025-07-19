e9807062c7fb2251c272bd839f6f5fded0dc2a72
Vivek Pisal <54746512+vivekpisal@users.noreply.github.com>
2025-07-19 11:43:38 +0530

    Time: 281 ms (21.64%), Space: 51.4 MB (83.23%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,28 +1,27 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        int n=nums.length;
         List<List<Integer>> threeSum = new ArrayList<>();
-        for(int i=0;i<n-2;i++){
-            while(i != 0 && i < n-2 && nums[i] == nums[i-1]) i++;
+        for(int i=0;i<nums.length-2;i++){
+            while(i != 0 && i < nums.length-2 && nums[i] == nums[i-1]) i++;
+            
+            for(int j=i+1;j<nums.length-1;j++){
+                while(j != i+1 && j < nums.length-1 && nums[j] == nums[j-1]) j++;
+            
+                int target = 0 - (nums[i] + nums[j]);
+                int start = j+1,end = nums.length-1;
 
-            for(int j=i+1;j<n-1;j++){
-                while(j != i+1 && j < n-1 && nums[j] == nums[j-1]) j++;
-
-                int target = nums[i] + nums[j];
-                int start=j+1,end=nums.length-1,mid=(start+end)/2;
-                
                 while(start <= end){
-                    mid=(start+end)/2;
-                    if(nums[mid] + target == 0){
+                    int mid = (start + end)/2;
+                    if(nums[mid] == target){
                         threeSum.add(List.of(nums[i],nums[j],nums[mid]));
                         break;
                     }
-                    else if(nums[mid] + target > 0){
-                        end=mid-1;
+                    else if(nums[mid] > target){
+                        end = mid-1;
                     }
-                    else if(nums[mid] + target < 0){
-                        start=mid+1;
+                    else if(nums[mid] < target){
+                        start = mid+1;
                     }
                 }
             }
