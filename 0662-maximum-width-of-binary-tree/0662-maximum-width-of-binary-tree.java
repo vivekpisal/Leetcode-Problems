4dac5629c038a7f6d6aa22a5e62de5db6f60e2f5
Vivek Pisal <54746512+vivekpisal@users.noreply.github.com>
2025-08-07 20:34:23 +0530

    Time: 1 ms (98.93%), Space: 43.7 MB (34.5%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -13,36 +13,41 @@
  *     }
  * }
  */
+
+class Pair{
+    TreeNode node;
+    int index;
+    Pair(TreeNode node,int index){
+        this.node = node;
+        this.index = index;
+    }
+}
 class Solution {
     public int widthOfBinaryTree(TreeNode root) {
         if(root == null) return 0;
-        Queue<TreeNode> queue = new LinkedList<>();
-        Queue<Integer> numbering = new LinkedList<>();
-        queue.offer(root);
-        numbering.offer(1);
-        int maxWidth=0;
+        Queue<Pair> queue = new LinkedList<>();
+        int maxWidth = Integer.MIN_VALUE;
+        queue.offer(new Pair(root,1));
         while(!queue.isEmpty()){
+            int first = 0,last = 0;
             int size = queue.size();
-            int leftIdx=0,rightIdx=0;
+            int mmin = queue.peek().index;
             for(int i=0;i<size;i++){
-                TreeNode top=queue.poll();
-                int idx=numbering.poll();
-                if(i == 0){
-                    leftIdx=idx;
-                } 
-                if(i == size-1){
-                    rightIdx=idx;
-                }
-                if(top.left != null){
-                    queue.offer(top.left);
-                    numbering.offer(2*idx);
-                }
-                if(top.right != null){
-                    queue.offer(top.right);
-                    numbering.offer(2*idx+1);
-                }
+                TreeNode node = queue.peek().node;
+                int index = queue.peek().index;
+                queue.poll();
+
+                if(i == 0)
+                    first = index;
+                if(i == size-1)
+                    last = index;
+                
+                if(node.left != null)
+                    queue.offer(new Pair(node.left,(index-mmin)*2+1));
+                if(node.right != null)
+                    queue.offer(new Pair(node.right,(index-mmin)*2+2));
             }
-            maxWidth = Math.max(maxWidth,rightIdx-leftIdx+1);
+            maxWidth = Math.max(last - first + 1,maxWidth);
         }
         return maxWidth;
     }
