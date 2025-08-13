34c2d55f5faa6ced2a4441b9725f4d81ee26ac0f
Vivek Pisal <54746512+vivekpisal@users.noreply.github.com>
2025-08-13 20:32:57 +0530

    Time: 6 ms (100%), Space: 44.6 MB (85.1%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -8,37 +8,16 @@
  * }
  */
 class Solution {
-    public boolean findPath(TreeNode node,List<TreeNode> path,TreeNode d){
-        if(node == null) return false;
-        if(node == d) {
-            path.add(node);
-            return true;
-        }      
-        path.add(node);
-        boolean ans = findPath(node.left,path,d);
-        if(ans)
-           return true;
-        boolean ans1 = findPath(node.right,path,d);
-        if(ans1)
-            return true;
-        path.remove(path.size()-1);
-        return false;
-    } 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        List<TreeNode> pathP = new ArrayList<>();
-        List<TreeNode> pathQ = new ArrayList<>();
-        findPath(root,pathP,p);
-        findPath(root,pathQ,q);
-        Map<TreeNode,Integer> map = new HashMap<>();
-        for(TreeNode i:pathP){
-            map.putIfAbsent(i,1);
+        if(root == null) return null;
+
+        TreeNode left,right;
+        if(root.val == p.val || root.val == q.val) return root;
+        else{
+            left = lowestCommonAncestor(root.left,p,q);
+            right = lowestCommonAncestor(root.right,p,q);
         }
-        for(int i=pathQ.size()-1;i>=0;i--){
-            if(map.containsKey(pathQ.get(i))){
-                return pathQ.get(i);
-            }
-        }
-        return null;
-    } 
-    
+        if(left != null && right != null) return root;
+        return left == null ? right : left;
+    }
 }
