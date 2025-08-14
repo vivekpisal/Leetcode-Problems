afe53fb4d7f5d7bf820c58f0209dac8a15ecf469
Vivek Pisal <54746512+vivekpisal@users.noreply.github.com>
2025-08-14 19:58:57 +0530

    Time: 8 ms (12.26%), Space: 49.5 MB (62.59%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,43 +1,45 @@
-class Solution {
-    private static class Pair{
-        int i,j;
-        Pair(int i,int j){
-            this.i=i;
-            this.j=j;
-        }
+class Pair{
+    int i,j;
+
+    public Pair(int i,int j){
+        this.i = i;
+        this.j = j;
     }
+}
+class Solution {
     public int numIslands(char[][] grid) {
-        if(grid.length == 0) return 0;
-        int n=grid.length,m=grid[0].length;
-        int next[] = {1,-1};
-        int numOfislands=0;
+        int numOfIsland = 0;
+        Queue<Pair> queue = new LinkedList<>();
+        int n = grid.length,m = grid[0].length;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j] == '1'){
-                    Queue<Pair> queue = new LinkedList<>();
                     queue.offer(new Pair(i,j));
-                    numOfislands++;
+                    grid[i][j] = '2';
                     while(!queue.isEmpty()){
-                        int row=queue.element().i;
-                        int col=queue.element().j;
-                        queue.poll();
-                        grid[row][col] = '2';
-                        for(int k=0;k<next.length;k++){
-                            int nextRow = row + next[k];
-                            int nextCol = col + next[k];
-                            if(nextRow >= 0 && nextRow < n && grid[nextRow][col] == '1'){
-                                queue.add(new Pair(nextRow,col));
-                                grid[nextRow][col] = '2';
-                            }
-                            if(nextCol >= 0 && nextCol < m && grid[row][nextCol] == '1'){
-                                queue.add(new Pair(row,nextCol));
-                                grid[row][nextCol] = '2';
-                            }
+                        Pair p = queue.poll();
+                        int x = p.i,y = p.j;
+                        if(x-1 >= 0 && grid[x-1][y] == '1'){
+                            grid[x-1][y] = '2';
+                            queue.offer(new Pair(x-1,y));
+                        }
+                        if(x+1 < n && grid[x+1][y] == '1'){
+                            grid[x+1][y] = '2';
+                            queue.offer(new Pair(x+1,y));
+                        }
+                        if(y-1 >= 0 && grid[x][y-1] == '1'){
+                            grid[x][y-1] = '2';
+                            queue.offer(new Pair(x,y-1));
+                        }
+                        if(y+1 < m && grid[x][y+1] == '1'){
+                            grid[x][y+1] = '2';
+                            queue.offer(new Pair(x,y+1));
                         }
                     }
+                    numOfIsland++;
                 }
             }
         }
-        return numOfislands;
+        return numOfIsland;
     }
 }
